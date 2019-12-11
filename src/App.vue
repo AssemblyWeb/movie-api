3df19c9c0623ecab513246c1562ef812b00ee44a
AssemblyWeb <alfio@assemblyweb.net>
2019-12-11 13:10:58 +0100

    search on mobile able

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse"
-        data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarColor01">
+      <a class="navbar-brand" href="#">Vue.js search movie</a>
+      <div class="" id="navbarColor01">
         <form @submit.prevent="getResults" class="form-inline my-2 my-lg-0">
           <input v-model="searchTerm" class="form-control mr-sm-2" type="text" placeholder="Search">
           <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
@@ -68,7 +63,7 @@ export default {
         this.error = data.Error;
       } else if (this.searchTerm === 'mandalorian') {
         this.results = data.Search;
-        this.errore = 'This is the way';
+        this.error = 'This is the way';
         // console.log(this.results);
       } else {
         this.results = data.Search;
