15b162678f328750ef4076d475aa72a1b65f750c
AssemblyWeb <alfio@assemblyweb.net>
2020-01-02 01:36:44 +0100

    add grid and cards to layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,70 @@
 <template>
   <div id="app">
-   <Navbar/>
-    <div class="container">
-      <div class="row mt-3">
-        <div class="col-9">
+    <nav class="flex items-center justify-between flex-wrap shadow-md p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">
+          <img src="@/assets/logo.svg" alt="">
+        </span>
+      </div>
+      <form class="" @submit.prevent="getResults">
+        <input v-model="searchTerm" class="transition focus:outline-0 border border-transparent focus:bg-white
+      focus:border-gray-300 placeholder-gray-600 rounded-lg bg-gray-200 py-2 pr-4 pl-10 block w-full
+      appearance-none leading-normal" type="text" placeholder="Search">
+      </form>
+      <div class="block flex-grow">
+        <a href="https://github.com/AssemblyWeb/vue-api" target="_blank"
+          class="float-right hover:text-white">
+          <img class="w-10" src="@/assets/github-brands.svg"
+            alt="https://github.com/AssemblyWeb/vue-api">
+        </a>
+      </div>
+    </nav>
+    <!-- testgrid -->
+    <!-- <div class=""> -->
+      <div class="flex flex-grow mt-5">
+        <div class="w-3/4 bg-gray-300 w-full">
+            first
+            <div class="flex flex-wrap">
+              <movie v-for="movie in results" :key="movie.id" class="" :movie="movie"
+                :addFav="addFav" :removeFav="removeFav">
+              </movie>
+            </div>
+        </div>
+        <div class="w-1/4 bg-gray-500">
+         second
+         <div v-for="movie in favs" :key="movie.id" class="flex">
+            {{movie.Title}} {{movie.Year}}
+            <a @click="removeFav(movie)" class="btn btn-primary">remove</a>
+          </div>
+        </div>
+      </div>
+    <!-- </div> -->
+    <!-- test grid -->
+    <!-- <div class="flex">
+      <div class="">
+        <div class="w-3/4">
           <div v-if="error" class="mt-3 alert alert-dismissible alert-danger">
             <strong>{{error}}</strong>
           </div>
-          <div class="row">
-            <movie v-for="movie in results" :key="movie.id" class="card col-4" :movie="movie"
+          <div class="flex">
+            <movie v-for="movie in results" :key="movie.id" class="w-1/3" :movie="movie"
               :addFav="addFav" :removeFav="removeFav">
             </movie>
           </div>
         </div>
-        <div class="col-3">
+        <div class="w-1/4">
           <div v-for="movie in favs" :key="movie.id">
             {{movie.Title}} {{movie.Year}}
             <a @click="removeFav(movie)" class="btn btn-primary">remove</a>
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 import Movie from '@/components/Movie.vue';
-import Navbar from '@/components/Navbar.vue';
 
 const API_URL = 'https://omdb-api.now.sh/?type=movie&s=';
 
@@ -34,7 +72,6 @@ export default {
   name: 'app',
   components: {
     Movie,
-    Navbar,
   },
   data: () => ({
     error: '',
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,16 +1,16 @@
 <template lang="html">
-    <div>
-        <div class="card-img-top movie-card"
-        v-bind:style='{ backgroundImage: "url(" + movie.Poster + ")", }'
-        >
-        </div>
-        <h5 class="card-title mt-2 font-weight-bold">{{movie.Title}}</h5>
-        <p class="card-text">
-            {{movie.Year}}
-        </p>
-        <a :href="'https://www.imdb.com/title/' + movie.imdbID" target="_blank" class="btn btn-danger">See movie</a>
-        <a @click="addFav(movie)" class="btn btn-primary mt-2">add</a>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img class="w-full" :src='movie.Poster'
+      alt="Sunset in the mountains">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{movie.Title}} ({{movie.Year}})</div>
+      <p class="text-gray-700 text-base">
+        <a :href="'https://www.imdb.com/title/' + movie.imdbID" target="_blank"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">See movie</a>
+        <a @click="addFav(movie)" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">add</a>
+      </p>
     </div>
+  </div>
 </template>
 
 <script>
